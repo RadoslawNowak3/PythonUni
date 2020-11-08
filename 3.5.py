8862f0c58449b6bc729eaf6e0e785622b793d751
Ziemian <ziemian123@gmail.com>
2020-11-08 17:27:49 +0100

    Corrected version

--- 3.5.py
+++ 3.5.py
@@ -1,4 +1,8 @@
-output = "|....|....|....|....|....|....|....|....|....|....|....|....| \n"
-for i in range(13):
-    output+= '{:5}'.format(str(i))
+length = int(input("Input measure length: "))
+output = "|"
+for i in range(length):
+    output += "....|"
+output += "\n"
+for i in range(length + 1):
+    output += '{:5}'.format(str(i))
 print(output)
